1b67d5cffa783913d2670fedca059a9e681f2165
Tom <woodwart@gmail.com>
2016-02-18 12:03:11 -0500

    install links to aliases and prompt #113307145

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 mkdir -p ~/.daftlabs
 curl -L https://github.com/daftlabs/dl-dev-profile/archive/master.tar.gz | tar -zx -C ~/.daftlabs --strip-components=1
+
+grep -q -F 'source ~/.daftlabs/bash_aliases' ~/.bash_profile || echo 'source ~/.daftlabs/bash_aliases' >> ~/.bash_profile
+grep -q -F 'source ~/.daftlabs/bash_prompt' ~/.bash_profile || echo 'source ~/.daftlabs/bash_prompt' >> ~/.bash_profile
