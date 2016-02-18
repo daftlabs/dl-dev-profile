4ff1e32f644bf5f6a5b6d158b737d1a703c1a7cb
Tom <woodwart@gmail.com>
2016-02-18 11:42:19 -0500

    lol typo #113307145

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/.daftlabs
-curl -L https://github.com/daftlabs/dl-dev-profile/archive/master.tar.gz | tar -zx -C ~/.dafltabs --strip-components=1
+curl -L https://github.com/daftlabs/dl-dev-profile/archive/master.tar.gz | tar -zx -C ~/.daftlabs --strip-components=1
