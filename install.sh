b142cc8b9f864a797322901127fcc4ec161d441a
Samuel Davis <samueljakdavis@gmail.com>
2016-06-15 10:58:30 -0400

    Installing aws, brew and mysql

--- install.sh
+++ install.sh
@@ -8,4 +8,14 @@ grep -q -F 'source ~/.daftlabs/bash_prompt' ~/.bash_profile || echo 'source ~/.d
 
 touch ~/.daftlabs/config
 
+#install AWS cli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install mysql
+brew install mysql
+
 source ~/.bash_profile
