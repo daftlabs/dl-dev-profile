ebf47ea98cbdc2fa0ca1ff5e7e0657c0dca3ce80
Samuel Davis <samueljakdavis@gmail.com>
2016-06-15 21:45:19 -0400

    adding gc alias, removing superfluous dSSH cmd

--- bash_profile.sh
+++ bash_profile.sh
@@ -18,8 +18,18 @@ function sha() {
   git rev-parse --short HEAD 2>/dev/null | sed -e "s/\(.*\)/\[\1\]/" || return
 }
 
-dSSH() {
-  docker exec -ti $1 /bin/bash
+gc() {
+  branch_name=`git symbolic-ref --short HEAD`
+  pivotal_id=`echo $branch_name | sed -En 's/^([0-9]{7,}).*$/\1/p'`
+  if [ -z "$pivotal_id" ]; then
+    if [ -z "$2" ]; then
+      echo 'No Pivotal Id detected in branch name, please supply pivotal ID and commit message'
+    else
+      git commit -m "[#$1] $2"
+    fi
+  else
+    git commit -m "[#$pivotal_id] $1"
+  fi
 }
 
 #exports
@@ -29,10 +39,11 @@ export TERM=xterm-256color
 
 #docker shortcuts
 #======================================================================
-RED="\[\e[0;31m\]"
+dssh() {
+  docker exec -ti $1 /bin/bash
+}
 alias drun="docker-compose run web"
 alias dash="docker-compose run web /bin/bash"
-alias dssh=dSSH
 alias dkill="docker kill \$(docker ps -q)"
 alias drmc="docker rm \$(docker ps -aq)"
 alias drmi="docker rmi \$(docker images -q)"
