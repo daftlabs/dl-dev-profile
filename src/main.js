b170ff4fa82adade3927bd1230a77ecc0cc93e54
Samuel Davis <samueljakdavis@gmail.com>
2016-07-11 09:14:49 -0400

    partial SSH command

--- src/main.js
+++ src/main.js
@@ -10,15 +10,17 @@ const dataStore = require('./services/dataStore')();
 utils.promisify(fs.readdir.bind(fs, COMMANDS_DIR))
   .then(_.map.bind(_, file => require(`${COMMANDS_DIR}/${file}`.replace(/\.js$/, ''))({vorpal})))
   .then(groups => _.reduce((group, commands) => commands.concat(group), [], groups)
-    .forEach(command => vorpal
-      .command(command.command)
-      .autocomplete(command.autocomplete)
-      .action(function (args, cb) {
-        return command.action(_.merge(args, {action: this}))
-          .then(res => res ? console.log(res) : console.error('No response defined.'))
-          .then(cb)
-          .catch(err => console.error(err.stack))
-      })));
+    .forEach(command => {
+      const cmd = vorpal.command(command.command);
+      (command.options || []).forEach(args => cmd.option.apply(cmd, args));
+      return cmd.autocomplete(command.autocomplete)
+        .action(function (args, cb) {
+          return command.action(_.merge(args, {action: this}))
+            .then(res => res ? console.log(res) : console.error('No response defined.'))
+            .then(cb)
+            .catch(err => console.error(err.stack))
+        })
+    }));
 
 dataStore.profiles
   .getCurrent()
