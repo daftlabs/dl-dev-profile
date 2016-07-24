af2bfc63a0ec1b35cbb0b9a7546f7997455943ca
Samuel Davis <samueljakdavis@gmail.com>
2016-07-24 19:58:18 -0400

    es6ifying describe

--- src/commands/describe.js
+++ src/commands/describe.js
@@ -9,33 +9,31 @@ module.exports = (config = {}) => {
     options: [
       ['-e, --events [n]', 'include n events', 3]
     ],
-    action: function (project, environment, command) {
-      return awsGateway
-        .describeService(`${project}-${environment}`)
-        .then(({service, tasks, taskDefinition, nodes}) => {
-          const environment = {};
-          const names = [taskDefinition.family, 'web', project];
-          (_.get('environment', _.find(_.partial(names.includes), taskDefinition.containerDefinitions)) || [])
-            .forEach(({name, value}) => environment[name] = value);
-          return JSON.stringify({
-            service: {
-              deployments: _.map(
-                _.pick(['desiredCount', 'runningCount', 'pendingCount', 'createdAt']),
-                service.deployments
-              ),
-              events: _.map(({createdAt, message}) => {
-                return {[createdAt]: message};
-              }, service.events.slice(0, command.events))
-            },
-            tasks: tasks.length,
-            taskDefinition: {
-              revision: taskDefinition.revision,
-              containers: _.map(_.pick(['name', 'image']), taskDefinition.containerDefinitions),
-              environment
-            },
-            nodes: _.map(_.pick(['PublicIpAddress']), nodes)
-          }, null, 2);
-        });
-    }
+    action: (project, environment, command) => awsGateway
+      .describeService(`${project}-${environment}`)
+      .then(({service, tasks, taskDefinition, nodes}) => {
+        const environment = {};
+        const names = [taskDefinition.family, 'web', project];
+        (_.get('environment', _.find(_.partial(names.includes), taskDefinition.containerDefinitions)) || [])
+          .forEach(({name, value}) => environment[name] = value);
+        return JSON.stringify({
+          service: {
+            deployments: _.map(
+              _.pick(['desiredCount', 'runningCount', 'pendingCount', 'createdAt']),
+              service.deployments
+            ),
+            events: _.map(({createdAt, message}) => {
+              return {[createdAt]: message};
+            }, service.events.slice(0, command.events))
+          },
+          tasks: tasks.length,
+          taskDefinition: {
+            revision: taskDefinition.revision,
+            containers: _.map(_.pick(['name', 'image']), taskDefinition.containerDefinitions),
+            environment
+          },
+          nodes: _.map(_.pick(['PublicIpAddress']), nodes)
+        }, null, 2);
+      })
   }];
 };
